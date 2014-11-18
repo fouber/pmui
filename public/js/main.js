e8db2dfc83e41a6b393309c824fbe3f0d2602ba0
fouber <fouber.NET@gmail.com>
2014-11-18 13:44:18 +0800

    [UI] remove thumb

--- public/js/main.js
+++ public/js/main.js
@@ -1,127 +1,122 @@
-(function(){
+(function () {
 
-    function getTimeString(num){
-        var d = new Date();
-        d.setTime(num);
-        var day = [
-            String(d.getFullYear()).substring(2),
-            pad(d.getMonth() + 1),
-            pad(d.getDate())
-        ].join('/');
-        var time = [
-            pad(d.getHours()),
-            pad(d.getMinutes()),
-            pad(d.getSeconds())
-        ].join(':');
-        return day + ' ' + time;
-    }
+  function getTimeString(num) {
+    var d = new Date();
+    d.setTime(num);
+    var day = [
+      String(d.getFullYear()).substring(2),
+      pad(d.getMonth() + 1),
+      pad(d.getDate())
+    ].join('/');
+    var time = [
+      pad(d.getHours()),
+      pad(d.getMinutes()),
+      pad(d.getSeconds())
+    ].join(':');
+    return day + ' ' + time;
+  }
 
-    function pad(str){
-        return ('0' + str).substr(-2);
-    }
+  function pad(str) {
+    return ('0' + str).substr(-2);
+  }
 
-    function getDiff(item, last, ext){
-        return [
-            item.root, 'diff',
-            last + '-' + item.time + '.' + ext
-        ].join('/');
-    }
+  function getDiff(item, last, ext) {
+    return [
+      item.root, 'diff',
+      last + '-' + item.time + '.' + ext
+    ].join('/');
+  }
 
-    var router = {
-        show: function(path){
-            $.get('/info', {path: path}, function(data){
-                if(data.status === 0){
-                    var html = '';
-                    var last;
-                    data.object.list.forEach(function(item){
-                        var attr = ' data-diff="' + (last ? getDiff(item, last, data.object.ext) : item.screenshot) + '"';
-                        var h = '<td>';
-                        h += '<div class="screenshot">';
-                        h += '<div class="title">' + getTimeString(item.time) + '</div>';
-                        h += '<div class="pic"' + attr + ' data-pic="' + item.screenshot + '">';
-                        h += '<img src="/' + item.screenshot + '">';
-                        h += '</div>';
-                        h += '</div>';
-                        h += '</td>';
-                        last = item.time;
-                        html += h;
-                    });
-                    html = '<table><tr>' + html + '</tr></table>';
-                    var list = $('#list');
-                    list.html(html);
-                    list.parent().scrollLeft(list.get(0).scrollWidth);
-                    $('#diff').html('');
-                } else {
-                    console.error(data.message);
-                }
-            }, 'json');
+  var router = {
+    show: function (path) {
+      $.get('/info', {path: path}, function (data) {
+        if (data.status === 0) {
+          var html = '';
+          var last;
+          data.object.list.forEach(function (item) {
+            var attr = ' data-diff="' + (last ? getDiff(item, last, data.object.ext) : item.screenshot) + '"';
+            var h = '';
+            h += '<div class="screenshot"' + attr + ' data-pic="' + item.screenshot + '">';
+            h += '<div class="title">' + getTimeString(item.time) + '</div>';
+            h += '</div>';
+            last = item.time;
+            html += h;
+          });
+          // html = '<table><tr>' + html + '</tr></table>';
+          var list = $('#list');
+          list.html(html);
+          list.parent().scrollLeft(list.get(0).scrollWidth);
+          $('#diff').html('');
+        } else {
+          console.error(data.message);
         }
-    };
-
-    function onhashchange(){
-        var hash = location.hash;
-        if(/^#!\//.test(hash)){
-            hash = hash.substring(3);
-            var pos = hash.indexOf('/');
-            var method = 'index';
-            var param = '';
-            if(pos > 0){
-                method = hash.substring(0, pos);
-                param = hash.substring(pos + 1);
-            }
-            if(router.hasOwnProperty(method)){
-                router[method](param);
-            }
-        }
+      }, 'json');
     }
-    if ('addEventListener' in window) {
-        window.addEventListener('hashchange', onhashchange, false);
+  };
+
+  function onhashchange() {
+    var hash = location.hash;
+    if (/^#!\//.test(hash)) {
+      hash = hash.substring(3);
+      var pos = hash.indexOf('/');
+      var method = 'index';
+      var param = '';
+      if (pos > 0) {
+        method = hash.substring(0, pos);
+        param = hash.substring(pos + 1);
+      }
+      if (router.hasOwnProperty(method)) {
+        router[method](param);
+      }
+    }
+  }
+
+  if ('addEventListener' in window) {
+    window.addEventListener('hashchange', onhashchange, false);
+  } else {
+    window.onhashchange = onhashchange;
+  }
+  onhashchange();
+
+  var list = $('#list');
+  //list.mousemove('.pic', function(e){
+  //    if(e.target.className === 'pic'){
+  //        var $this = $(e.target);
+  //        var h = $this.height();
+  //        var rY = e.offsetY / h;
+  //        var img = $this.find('img');
+  //        var H = img.height() - h;
+  //        var y = -rY * H;
+  //        img.css('transform', 'translate3d(0, ' + y + 'px, 0)');
+  //    }
+  //});
+  //list.mouseout('.pic', function(e){
+  //    if(e.target.className === 'pic'){
+  //        var $this = $(e.target);
+  //        var img = $this.find('img');
+  //        img.css('transform', 'translate3d(0, 0, 0)');
+  //    }
+  //});
+  list.contextmenu(function (e) {
+    e.stopPropagation();
+    e.preventDefault();
+  });
+  list.on('mousedown', '.screenshot', function (e) {
+    list.find('.screenshot').removeClass('actived');
+    var $this = $(this);
+    $this.addClass('actived');
+    var pic = $this.attr('data-pic');
+    if (e.button == '2') {
+      $('#diff').html('<img src="/' + pic + '">');
     } else {
-        window.onhashchange = onhashchange;
+      var diff = $this.attr('data-diff');
+      if (diff) {
+        $('#diff').html('<img src="/' + diff + '">');
+      } else {
+        $('#diff').html('<span>没有对比数据</span>');
+      }
     }
-    onhashchange();
-
-    var list = $('#list');
-    list.mousemove('.pic', function(e){
-        if(e.target.className === 'pic'){
-            var $this = $(e.target);
-            var h = $this.height();
-            var rY = e.offsetY / h;
-            var img = $this.find('img');
-            var H = img.height() - h;
-            var y = -rY * H;
-            img.css('transform', 'translate3d(0, ' + y + 'px, 0)');
-        }
-    });
-    list.mouseout('.pic', function(e){
-        if(e.target.className === 'pic'){
-            var $this = $(e.target);
-            var img = $this.find('img');
-            img.css('transform', 'translate3d(0, 0, 0)');
-        }
-    });
-    list.contextmenu(function(e){
-        e.stopPropagation();
-        e.preventDefault();
-    });
-    list.mousedown('.pic', function(e){
-        if(e.target.className === 'pic'){
-            list.find('.screenshot').removeClass('actived');
-            var $this = $(e.target);
-            $this.parent().addClass('actived');
-            var pic = $this.attr('data-pic');
-            if(e.button == '2'){
-                $('#diff').html('<img src="/' + pic + '">');
-            } else {
-                var diff = $this.attr('data-diff');
-                if(diff){
-                    $('#diff').html('<img src="/' + diff + '">');
-                } else {
-                    $('#diff').html('<span>没有对比数据</span>');
-                }
-            }
-            e.preventDefault();
-            e.stopPropagation();
-        }
-    });
+    e.preventDefault();
+    e.stopPropagation();
+  });
 })();
